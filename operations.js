a0974923005ca4bd95e3e3e9d63e8a5dc999b6e0
shreyansh0009 <saurabhsharraf@gmail.com>
2024-02-27 22:16:29 +0530

    day-2

---
+++ operations.js
@@ -0,0 +1,22 @@
+
+let str1 = "Hello"
+
+let str2 = " Saurabh"
+
+console.log(str1 + str2); // String concatanation
+
+//************************************************************ */
+
+console.log("1" + 8);  // OP = 18
+
+console.log(1 + "8");  // OP = 18
+
+console.log("1" + 8 + 8);  // OP = 188
+
+console.log(1 + 8 + "8");  // OP = 98
+
+// Conclusion: String is priority in JavaScript.
+
+// Agar pahle string aa gya too sb string ki tarah treet hoga, aur agar last me string aaya to pahle operation hoga phir string add hoga.
+
+//*************************************************************** */
